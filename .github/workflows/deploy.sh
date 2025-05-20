4f77571dbce03027da44a1153630f5e72657dcb8
Meghs0205 <162950793+Meghs0205@users.noreply.github.com>
2025-05-20 20:16:21 +0530

    Update deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -5,4 +5,9 @@ docker rm flask-cicd-demo || true
 
 echo "${GITHUB_TOKEN}" | docker login ghcr.io -u "${GITHUB_USER}" --password-stdin
 
+# 🔥 Pull latest image explicitly
+docker pull ghcr.io/${GITHUB_USER}/ci-cd-test:latest
+
+# Run latest image
 docker run -d -p 5000:5000 --name flask-cicd-demo ghcr.io/${GITHUB_USER}/ci-cd-test:latest
+
