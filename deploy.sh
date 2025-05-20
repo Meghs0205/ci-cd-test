c63b53bcd2888e57fdc7fe6f5bb951bea48c0cca
Meghs0205 <162950793+Meghs0205@users.noreply.github.com>
2025-05-20 18:15:38 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker stop flask-cicd-demo || true
-docker rm flask-cicd-demo || true
-
-echo "${GITHUB_TOKEN}" | docker login ghcr.io -u "${GITHUB_USER}" --password-stdin
-
-docker run -d -p 5000:5000 --name flask-cicd-demo ghcr.io/${GITHUB_USER}/ci-cd-test:latest
